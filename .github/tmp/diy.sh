28c2dc1796bd37aa65781d7ed9847d1d17853152
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-07 19:43:07 +0800

    m

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -98,10 +98,11 @@ sed -i 's/luci-lib-ipkg/luci-base/g' package/diy1/istore/luci-app-store/Makefile
 # svn export https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
 
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf package/mosdns/mosdns
+#rm -rf package/mosdns/mosdns
 
 # alist 
 git clone https://github.com/sbwml/luci-app-alist package/alist
