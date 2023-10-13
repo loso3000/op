c974856c3fc2869f2c20706852640b3c8605150e
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-14 02:25:13 +0800

    u

--- .github/tmp/package/emortal2/autocore/files/generic/10_system.js
+++ .github/tmp/package/emortal2/autocore/files/generic/10_system.js
@@ -3,6 +3,10 @@
 'require fs';
 'require rpc';
 
+var callLuciVersion = rpc.declare({
+	object: 'luci',
+	method: 'getVersion'
+});
 var callSystemBoard = rpc.declare({
 	object: 'system',
 	method: 'board'
@@ -44,7 +48,7 @@ return baseclass.extend({
 			L.resolveDefault(callCPUInfo(), {}),
 			L.resolveDefault(callCPUUsage(), {}),
 			L.resolveDefault(callTempInfo(), {}),
-			fs.lines('/usr/lib/lua/luci/version.lua')
+			L.resolveDefault(callLuciVersion(), { revision: _('unknown version'), branch: 'LuCI' })
 		]);
 	},
 
@@ -57,11 +61,7 @@ return baseclass.extend({
 		    tempinfo    = data[5],
 		    luciversion = data[6];
 
-		luciversion = luciversion.filter(function(l) {
-			return l.match(/^\s*(luciname|luciversion)\s*=/);
-		}).map(function(l) {
-			return l.replace(/^\s*\w+\s*=\s*['"]([^'"]+)['"].*$/, '$1');
-		}).join(' ');
+		luciversion = luciversion.branch + ' ' + luciversion.revision;
 
 		var datestr = null;
 
@@ -92,7 +92,7 @@ return baseclass.extend({
 				systeminfo.load[1] / 65535.0,
 				systeminfo.load[2] / 65535.0
 			) : null,
-			_('CPU usage (%)'),    cpuusage.cpuusage
+			_('CPU usage'),    cpuusage.cpuusage
 		];
 
 		if (tempinfo.tempinfo) {
