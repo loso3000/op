bc3d3e8c1a25d5ccf29937914dba030bf0b59621
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-15 21:14:48 +0800

    b

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -409,8 +409,8 @@ svn export https://github.com/fw876/helloworld/trunk/shadow-tls package/new/shad
 
 #bypass
 svn export https://github.com/loso3000/other/trunk/up/pass ./package/pass
-rm ./package/pass/luci-app-bypass/po/zh_Hans && mv ./package/pass/luci-app-bypass/po/zh-cn ./packag/pass/luci-app-bypass/po/zh_Hans
-rm ./package/pass/luci-app-ssr-plus/po/zh_Hans && mv ./package/pass/luci-app-ssr-plus/po/zh-cn ./packag/pass/luci-app-ssr-plus/po/zh_Hans
+rm ./package/pass/luci-app-bypass/po/zh_Hans && mv ./package/pass/luci-app-bypass/po/zh-cn ./package/pass/luci-app-bypass/po/zh_Hans
+rm ./package/pass/luci-app-ssr-plus/po/zh_Hans && mv ./package/pass/luci-app-ssr-plus/po/zh-cn ./package/pass/luci-app-ssr-plus/po/zh_Hans
 # sed -i 's,default n,default y,g' package/luci-app-bypass/Makefile
 
 
