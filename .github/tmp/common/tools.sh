32a6e2f1cb15e7c836a514384d69f31d6f64194b
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2025-01-12 09:03:12 +0800

    v

--- .github/tmp/common/tools.sh
+++ .github/tmp/common/tools.sh
@@ -22,3 +22,21 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/p
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 popd
+
+## opkg ##
+PLATFORM=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '"' '{print $2}')
+TARGET=$(cat .config | grep CONFIG_TARGET_BOARD | awk -F '"' '{print $2}')
+SUBTARGET=$(cat .config |  grep CONFIG_TARGET | sed -n 2p | awk -F '=' '{print $1}')
+
+mkdir -p files/etc/opkg
+pushd files/etc/opkg
+cat <<-EOF > "distfeeds.conf"
+src/gz ezopwrt_core https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/targets/x86/64/packages
+src/gz ezopwrt_base https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/base
+src/gz ezopwrt_luci https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/luci
+src/gz ezopwrt_packages https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/packages
+src/gz ezopwrt_routing https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/routing
+src/gz ezopwrt_telephony https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/telephony
+EOF
+cp distfeeds.conf distfeeds.conf.bak
+popd
