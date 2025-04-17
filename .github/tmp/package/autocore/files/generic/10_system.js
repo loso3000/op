fd201cc7b2ba96fe06b0d7710cce37d32ceb6047
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-04-17 12:14:03 +0800

    x

--- .github/tmp/package/autocore/files/generic/10_system.js
+++ .github/tmp/package/autocore/files/generic/10_system.js
@@ -103,7 +103,7 @@ return baseclass.extend({
 				E('td', { 'class': 'td right', 'width': '33%' }, [ fields[i] ]),
 				E('td', { 'class': 'td left' }, [ (fields[i + 1] != null) ? fields[i + 1] : '?' ])
 			]));
-		};
+		}
         table.appendChild(E('tr', { 
             'class': 'tr' 
         }, [
